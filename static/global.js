dd852acab7b7de295343d2fce7cc74ddc561781f
chm011 <chm011@ucsd.edu>
2024-10-25 03:18:31 -0700

    Update global.js

--- static/global.js
+++ static/global.js
@@ -7,7 +7,7 @@ function $$(selector, context = document) {
 
 // Define the pages for the navigation menu
 let pages = [
-  { url: '/my-portfolio', title: 'Home' },
+  { url: '/my-portfolio/', title: 'Home' },
   { url: '/my-portfolio/projects', title: 'Projects' },
   { url: '/my-portfolio/resume', title: 'Resume' },
   { url: '/my-portfolio/contacts', title: 'Contacts' },
