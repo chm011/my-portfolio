29713063a9b7f9c74e4c5116ccff294c5a8627fc
chm011 <chm011@ucsd.edu>
2024-10-25 03:02:52 -0700

    #1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Christie Ma</h1>
+
+
+<p>Current student in the Masters of Data Science program. I studied pharmacy in previous years and is hoping to combine my strengths and work in healthcare analytics or the consulting field. </p>
+<img 
+  src="/PHOTO-2023-04-03-22-38-17.jpg" alt="Profile Photo"  width="200" height="270" 
+/>
---
+++ src/routes/contacts/+page.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+  <title>Contact me</title>
+</svelte:head>
+
+<h1>Contacts</h1>
+
+<form action="mailto:chm011@ucsd.edu" method="POST" enctype="text/plain">
+    <label for="email">Email:</label>
+    <input name="email" type="email">
+
+    <label for="Subject">Subject:</label>
+    <input name="subject">
+
+    <label for="Body">Body:</label>
+    <textarea name="body"></textarea>
+    
+    <button>Submit</button>
+</form>
+
