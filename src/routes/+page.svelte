96d3fd4dafe34ba818ff5dfad0ff07d6cb4a335d
chm011 <chm011@ucsd.edu>
2024-11-01 16:58:06 -0700

    lab 5 step2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,3 +17,64 @@
   <Project data ={project} hLevel=3/>
   {/each}
 </div>
+
+
+{#await fetch("https://api.github.com/users/chm011") }
+{:then response}
+  {#await response.json() then profileData}
+    <section>
+      <h2>Profile Data</h2>
+      <dl class="profile-stats">
+        <div>
+          <dt>Followers</dt>
+          <dd>{profileData.followers}</dd>
+        </div>
+        <div>
+          <dt>Following</dt>
+          <dd>{profileData.following}</dd>
+        </div>
+        <div>
+          <dt>Public Repos</dt>
+          <dd>{profileData.public_repos}</dd>
+        </div>
+        <div>
+          <dt>Public Gists</dt>
+          <dd>{profileData.public_gists}</dd>
+        </div>
+      </dl>
+      </section>
+  {/await}
+{:catch error}
+<p class="error">Something went wrong: {error.message}</p>
+{/await}
+
+
+<style>
+    .profile-stats {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1em; 
+    text-align: left;
+  }
+
+  .profile-stats dt {
+    font-weight: bold;
+    color: #333;
+    grid-row: 1; 
+  }
+
+  .profile-stats dd {
+    font-size: 1.2em;
+    color: #555;
+    grid-row: 2; 
+    margin: 0;
+  }
+
+  section {
+    margin: 2em 0;
+    padding: 1em;
+  }
+
+
+
+</style>
