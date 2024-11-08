8d7b556099980620195edd343c8249efbe43cc2f
chm011 <156361082+chm011@users.noreply.github.com>
2024-11-08 13:37:13 -0800

    Update Project.svelte

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -11,16 +11,7 @@ export let hLevel =2;
     <img src={data.image} alt={data.title} width=300px height=200px/>
     <div id = 'project_description'>
     <p>{data.description}</p>
-    <p>Created on {data.year}</p>
+    <p><span style="font-family: Baskerville; font-variant-numeric: oldstyle-nums;">Created on {data.year}</p>
     </div>
 </article>
-
-<style>
-#project_description {
-    font-family:Baskerville;
-    font-variant-numeric: oldstyle-nums;
-}
-
-
-</style>
 
