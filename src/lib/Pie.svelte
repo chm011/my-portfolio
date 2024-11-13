8fecfee267d187010b3809fda8fb51fd28d4ffa6
chm011 <156361082+chm011@users.noreply.github.com>
2024-11-13 14:08:02 -0800

    6.3

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -7,31 +7,18 @@ let arc = arcGenerator({
   endAngle: 2 * Math.PI,
 });
 
-
-let data = [1, 2];
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
-
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
-let colors = ['gold', 'purple'];
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 </script>
 
 
 <svg viewBox="-50 -50 100 100">
     {#each arcs as arc, i}
-    <path d="{arc}" fill="{colors[i]}" />
+    <path d="{arc}" fill="{colors(i)}" />
     {/each}
 </svg>
 
