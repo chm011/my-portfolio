d0fece9a24ac54a49e78ad1746d29ca0cc5f3073
chm011 <156361082+chm011@users.noreply.github.com>
2024-11-13 13:21:51 -0800

    Update Pie.svelte

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -1,1 +1,14 @@
-Hello from Pie.svelte
+<svg viewBox="-50 -50 100 100">
+    <circle cx="0" cy="0" r="50" fill="red" />
+  </svg>
+
+
+<style>
+    svg {
+  max-width: 20em;
+  margin-block: 2em;
+
+  /* Do not clip shapes outside the viewBox */
+  overflow: visible;
+}
+</style>
