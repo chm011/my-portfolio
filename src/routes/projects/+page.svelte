00c1402d3208ba40378aef9fbdf6da93bc22ae46
chm011 <156361082+chm011@users.noreply.github.com>
2024-11-13 15:19:25 -0800

    Update +page.svelte

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,13 +4,13 @@
    import Pie from '$lib/Pie.svelte';
    import * as d3 from 'd3';
 
-  let rolledData = d3.rollups(
+  $: rolledData = d3.rollups(
     projects,
     (v) => v.length,
     (d) => d.year,
     );
   
-  let pieData = rolledData.map(([year, count]) => {
+  $: pieData = rolledData.map(([year, count]) => {
   return { value: count, label: year };
 });
 
