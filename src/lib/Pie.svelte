84d3d6b2a30cdc284750ccb0158837e63ff66596
chm011 <156361082+chm011@users.noreply.github.com>
2024-11-13 13:34:07 -0800

    Update Pie.svelte

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -1,6 +1,44 @@
+<script>
+import * as d3 from 'd3';
+
+let arc = d3.arc().innerRadius(0).outerRadius(50)({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+let data = [1, 2];
+
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = ['gold', 'purple'];
+</script>
+
+
 <svg viewBox="-50 -50 100 100">
-    <circle cx="0" cy="0" r="50" fill="red" />
+    <path d="{arc}" fill="red" />
   </svg>
+
+
+{#each arcs as arc}
+    <path d="{" arc } fill="{" colors[i] } />
+{/each}
+
+
 
 
 <style>
