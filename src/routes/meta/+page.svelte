0f71a1b4ecf6cbde39b03ecd2629fc5bc364e61b
chm011 <156361082+chm011@users.noreply.github.com>
2024-11-15 15:31:29 -0800

    Update +page.svelte

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -13,12 +13,21 @@ onMount(async () => {
   data = await d3.csv('loc.csv');
 });
 
+console.log(data)
 
-
+data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+  
 </script>
 
 
 <p>This is a description of the website.</p>
 <p>Total lines of code: {data.length}</p>
-console.log(data)
+
 
